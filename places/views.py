dd95cb0aaf534d1929c333c08d5a68547a832b9d
chio-san <32412086+chio-san@users.noreply.github.com>
2021-11-14 02:58:30 +0600

    add places model, views, urls

--- places/views.py
+++ places/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Place
 
+def places(request):
+    place_objects = Place.objects.all()
+    return render(request, 'places.html', {'places': place_objects})
 # Create your views here.
