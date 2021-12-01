c70aad01b36ec8ea6def5533e4f384ce746c3d11
chio-san <32412086+chio-san@users.noreply.github.com>
2021-12-01 23:26:04 +0600

    modified settings for pythonanywhere

--- Tourist/settings.py
+++ Tourist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7#s(0w0!+c=fi3&c*s5c(!0sycxp4yz!nx+5h=qe^80$f)_3x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
